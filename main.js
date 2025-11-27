192b95f9a8ef6cc53a09ca2c58b346e7acd552db
Ishan-Madhusanka <ishanmadhusanka594@gmail.com>
2025-11-27 22:15:01 +0530

    10th day

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: "#prev",
     },
 });
+
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+
+cartIcon.addEventListener('click',()=>cartTab.classList.add('cart-tab-active'));
