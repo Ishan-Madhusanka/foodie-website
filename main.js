e5c6a6fdea1b191292756a19bb8d32c9d85d527a
Ishan-Madhusanka <ishanmadhusanka594@gmail.com>
2025-11-29 23:39:22 +0530

    12days

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".mySwiper", {
-    loop:true,
+    loop: true,
     navigation: {
         nextEl: "#next",
         prevEl: "#prev",
@@ -9,45 +9,106 @@ var swiper = new Swiper(".mySwiper", {
 
 const cartIcon = document.querySelector('.cart-icon');
 const cartTab = document.querySelector('.cart-tab');
-const closeBtn = document.querySelector('.close-btn'); 
+const closeBtn = document.querySelector('.close-btn');
 const cardList = document.querySelector('.card-list');
+const cartList = document.querySelector('.cart-list');
 
-cartIcon.addEventListener('click',()=> cartTab.classList.add('cart-tab-active'));
-closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+cartIcon.addEventListener('click', () => cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener('click', () => cartTab.classList.remove('cart-tab-active'));
 
 
 let productList = [];
+let cartProduct = [];
 
-const showCards = () =>{
+const showCards = () => {
 
-    productList.forEach(product =>{
+    productList.forEach(product => {
 
         const orderCard = document.createElement('div');
         orderCard.classList.add('order-card');
 
         orderCard.innerHTML = `
         <div class="card-image">
-                            <img src="images/burger.png">
-                        </div>
-                        <h4>Double Beef Burger</h4>
-                        <h4 class="price">$200</h4>
-                        <a href="#" class="btn">Add to Cart</a>
+                 <img src="${product.image}">
+         </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a href="#" class="btn card-btn">Add to Cart</a>
         `;
 
         cardList.appendChild(orderCard);
 
+        const cardBtn =  orderCard.querySelector('.card-btn');
+
+        cardBtn.addEventListener('click',(e)=>{
+             e.preventDefault();
+             addToCart(product);
+
+        });
+    });
+};
+
+const addToCart = (product) =>{
+
+    const existingProduct = cartProduct.find(item=> item.id === product.id);
+    if(existingProduct){
+
+        alert('Item already in your cart!');
+        return;
+    }
+
+    cartProduct.push(product);
+
+    let quantity = 1;
+    let price = parseFloat(product.price.replace('$',''))
+
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('item');
+
+    cartItem.innerHTML = `
+    
+     <div class="item-image">
+         <img src="${product.image}">
+    </div>
+     <div class="detail">
+        <h4>${product.name}</h4>
+        <h4 class="item-total">${product.price}</h4>
+    </div>
+
+    <div class="flex">
+        <a href="#" class="quantity-btn minus">
+            <i class="fa-solid fa-minus"></i>
+        </a>
+        <h4 class="quantity-value">${quantity}</h4>
+         <a href="#" class="quantity-btn plus">
+            <i class="fa-solid fa-plus"></i>
+        </a>
+    </div>
+    `;
+
+    cartList.appendChild(cartItem);
+
+    const plusBtn = cartItem.querySelector('.plus');
+    const quantityValue =cartItem.querySelector('.quantity-value');
+    const itemTotel = cartItem.querySelector('.item-total');
+
+    plusBtn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        quantity++;
+        quantityValue.textContent = quantity;
+        itemTotel.textContent = `$${price * quantity}`
     })
 }
 
-const initApp = () =>{
+const initApp = () => {
 
     fetch('products.json').then
-    (response => response.json()).then
-    (data =>{
+        (response => response.json()).then
+        (data => {
 
-        productList = data;
-        showCards();
-    })
+            productList = data;
+            showCards();
+        })
 }
 
 initApp();
